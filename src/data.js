8f32c4f11a2b00e5164313af6af047321d73e3e0
Camila <cm.maschion@gmail.com>
2019-09-16 13:40:40 -0300

    funcao filter

--- src/data.js
+++ src/data.js
@@ -1,23 +1,48 @@
-// esta é uma função de exemplo
-// veja como agregamos a função ao objeto global window
 
-const example = () => {
-  return "example";
-};
+//personagem = fetch('../src/data/pokemon/pokemon.json').then(function(response) {
+ //    return response.json();
+ //   });
+ 
+ let personagens = POKEMON.pokemon;
 
-window.example = example;
-
-
-document.getElementById("filter").addEventListener('click', function(){
-  const types = Array.from(document.querySelectorAll(".type:checked")).map(function (element){
-    return element.value
-  })
-  filtrarPorTipo(types)
-     }
-);
-
-
-
+ function carregarPokemon() {
+ 
+     const showPokemons = document.getElementById("main");
+     showPokemons.innerHTML = "";
+     for (pokemons of personagens) {
+         showPokemons.innerHTML += `
+ <div id="pokemon" class="poke-card">
+ 
+ <div>
+ ${pokemons.name}
+ </div>
+ 
+ <img src ="${pokemons.img}"/>
+ </div>`;
+ 
+     };
+ }
+ 
+ carregarPokemon()
+ 
+ function filtrarPorTipo (types){
+   let pokeTipos = []
+   for (let i = 0; i < types.length; i++) {
+     const type = types[i]; 
+     personagens = POKEMON.pokemon;
+     personagens.map(function (personagem){
+         if (personagem.type.includes(type)) {
+             pokeTipos.push(personagem)         
+         }
+         
+     });     
+    }
+   personagens = pokeTipos 
+   carregarPokemon()
+ };
+ 
+ 
+ 
 
 
 
--- src/main.js
+++ src/main.js
@@ -1,58 +1,11 @@
-/*const POKEMONS = POKEMON.pokemon;
-const img = document.getElementById("imagens");
-
-POKEMONS.map(poke => img.innerHTML += `
-<div id="pokemon" class="pokeCard">
-
-    <div>
-    ${poke.name}
-    </div>
-    
-<img src=" ${poke.img}"/>
-</div>`);*/
-
-let personagens = POKEMON.pokemon;
-
-function carregarPokemon() {
-
-    const showPokemons = document.getElementById("main");
-    showPokemons.innerHTML = "";
-    for (pokemons of personagens) {
-        showPokemons.innerHTML += `
-<div id="pokemon" class="poke-card">
-
-<div>
-${pokemons.name}
-</div>
-
-<img src ="${pokemons.img}"/>
-</div>`;
-
-    };
-}
-
-carregarPokemon()
-
-function filtrarPorTipo (types){
-  let pokeTipos = []
-  for (let index = 0; index < types.length; index++) {
-    const type = types[index]; 
-    personagens = POKEMON.pokemon;
-    personagens.map(function (personagem){
-        if (personagem.type.includes(type)) {
-            pokeTipos.push(personagem)         
-        }
-        
-    });
-    
-    //console.log(pokemonSelecionado.name)
-  }
-  personagens = pokeTipos 
-  carregarPokemon()
-};
-
-
-
+document.getElementById("filter").addEventListener('click', function(){
+    const types = Array.from(document.querySelectorAll(".type:checked")).map(function (element){
+      return element.value
+    })
+    filtrarPorTipo(types)
+       }
+  );
+  
 
 
 
