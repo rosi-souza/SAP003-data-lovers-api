edcacc4b3266e108a8cddff4a5376efc1ced9f76
elaineshimabukuro <laineyoshizato@gmail.com>
2019-09-26 08:27:15 -0300

    teste pokeCalc

--- test/data.spec.js
+++ test/data.spec.js
@@ -103,15 +103,24 @@ describe("filtrarPorTipo", () => {
   });
 });
 
-describe("porcentagemPokemon", () => {
+/*describe("porcentagemPokemon", () => {
   it("is a function", () => {
     expect(typeof app.pokePorcentagem).toBe("function");
 
     it("returns `porcentagem pokemons`", () => {
       expect(app.pokePorcentagem(app.filtrarPorTipo bancoPokemons, "Fire")).toBe([{(2*100)/5
-     
+
       }
     ]
     );
     });});
-  });
+  });*/
+
+  describe("pokeCalc", () => {
+    it("is a function", () => {
+      expect(typeof app.pokeCalc).toBe("function");
+    });
+
+    it("returns `numero de pokemons por tipo`", () => {
+      expect(app.pokeCalc(bancoPokemons, countTypes)).toEqual({Grass:3;Poison:3;Fire:2});
+      });
