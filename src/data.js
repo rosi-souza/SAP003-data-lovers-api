041d2385afc8cd9ce408356d7bcf4450485b50f4
rosi-souza <rosi.souza@live.com>
2019-11-07 17:18:20 -0300

    adicionando pokeapi

--- src/data.js
+++ src/data.js
@@ -1,65 +1,66 @@
-window.app = {
-  filtrarPorTipo,
-  ordenPokemons,
-  pokeCalc
-};
+// window.app = {
+//   filtrarPorTipo,
+//   ordenPokemons,
+//   pokeCalc
+// };
 
-//filtrar pokemon
 
-function filtrarPorTipo(pokemon, types) {
-  let pokeTipos = []; // recebe/adiciona o pokemon filtrado
-  for (let i = 0; i < types.length; i++) { //entra no array que guarda os tipos de pokemon
-    const type = types[i];
-    pokemon.map(function (personagem) { //procura dentro do "for" o tipo de pokemon selecionado no checkbox
-      if (personagem.type.includes(type)) { // determinamos se realmente o array contem o tipo de pokemon selecionado
-        pokeTipos.push(personagem); //retorna então adicionando o pokemon selecionado ao array
-      }
-    });
-  }
-  return pokeTipos;
+// //filtrar pokemon
 
-}
+// function filtrarPorTipo(pokemon, types) {
+//   let pokeTipos = []; // recebe/adiciona o pokemon filtrado
+//   for (let i = 0; i < types.length; i++) { //entra no array que guarda os tipos de pokemon
+//     const type = types[i];
+//     pokemon.map(function (personagem) { //procura dentro do "for" o tipo de pokemon selecionado no checkbox
+//       if (personagem.type.includes(type)) { // determinamos se realmente o array contem o tipo de pokemon selecionado
+//         pokeTipos.push(personagem); //retorna então adicionando o pokemon selecionado ao array
+//       }
+//     });
+//   }
+//   return pokeTipos;
 
-//ordenarpokemon
+// }
 
-function ordenPokemons(orderPokemon, pokemons) {
-  let orderList = [];
+// //ordenarpokemon
 
-  switch (orderPokemon) {
-  case "a-z":
-    pokemons.sort((a, b) => {
-      if (a.name > b.name) {
-        return 1;
-      }
-      return -1;
-    });
-    break;
-  case "z-a":
-    pokemons.sort((a, b) => {
-      if (a.name > b.name) {
-        return 1;
-      }
-      return -1;
-    });
-    pokemons.reverse();
-    break;
-  }
-  return pokemons;
-};
+// function ordenPokemons(orderPokemon, pokemons) {
+//   // let orderList = [];
 
-//Calcula quantos pokemons tem em cada tipo
+//   switch (orderPokemon) {
+//   case "a-z":
+//     pokemons.sort((a, b) => {
+//       if (a.name > b.name) {
+//         return 1;
+//       }
+//       return -1;
+//     });
+//     break;
+//   case "z-a":
+//     pokemons.sort((a, b) => {
+//       if (a.name > b.name) {
+//         return 1;
+//       }
+//       return -1;
+//     });
+//     pokemons.reverse();
+//     break;
+//   }
+//   return pokemons;
+// };
 
-function pokeCalc(pokeData) {
-  let countTypes = pokeData.reduce(function (acumulador, pokemon) {
-    for (tipoPokemon of pokemon.type) {
-      if (tipoPokemon in acumulador) {
-        acumulador[tipoPokemon]++;
-      } else {
-        acumulador[tipoPokemon]=1;
-      }
-    }
+// //Calcula quantos pokemons tem em cada tipo
 
-    return acumulador;
-  }, {});
-  return countTypes;
-};
+// function pokeCalc(pokeData) {
+//   let countTypes = pokeData.reduce(function (acumulador, pokemon) {
+//     for (tipoPokemon of pokemon.type) {
+//       if (tipoPokemon in acumulador) {
+//         acumulador[tipoPokemon]++;
+//       } else {
+//         acumulador[tipoPokemon]=1;
+//       }
+//     }
+
+//     return acumulador;
+//   }, {});
+//   return countTypes;
+// };
--- src/main.js
+++ src/main.js
@@ -1,65 +1,92 @@
-let personagens = POKEMON.pokemon;
+// let personagens = POKEMON.pokemon;
+const showPokemons = document.getElementById("principal");
 
-function carregarPokemon(pokemons) {
-  personagens = pokemons;
-
-  const showPokemons = document.getElementById("principal");
+const getPokemon = () => {
   let layout = "";
   showPokemons.innerHTML = "";
-  for (pokemon of pokemons) {
-    layout += `
-          <div id="pokemon" class="poke-card">
-            <div class="infos">
-              <img src ="${pokemon.img}"/>
-              <div id = "name">
-                ${pokemon.name}
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+    .then(response => response.json())
+    .then((data) => {
+      const dataBase = data.results;
+      dataBase.map(x => {
+        layout += `
+            <div id="pokemon" class="poke-card">
+              <div class="infos">
+                <div id = "name">
+                  ${x.name}
               </div>
               <p class="subtitle">Tipo:</p>
-              ${pokemon.type}
               <p class="subtitle">Fraquezas:</p>
-              ${pokemon.weaknesses}
             </div>
-          </div>`;
-  };
-  showPokemons.innerHTML = layout;
-}
-carregarPokemon(personagens);
+          `;
+        showPokemons.innerHTML = layout;
+      });
+    });
+};
 
-document.getElementById("filter").addEventListener("click", function() {
-  const types = Array.from(document.querySelectorAll(".type:checked")).map(function (element) {
-    //foi criado um array, no qual estão os pokemon filtrados, a partir do array onde estão todos os pokemon
-    //queryselectorall retornou uma lista com os elementos especificados(tipos checados pelo usuário)
-    // map foi chamado para buscar o tipo dentro desse novo array
-    return element.value;
-  });
-  filtrarPorTipo(POKEMON.pokemon, types);
-  const pokemonFiltrado = app.filtrarPorTipo(POKEMON.pokemon, types);
+// <p> ${dataBase.map(x => x.name)} </p>
+getPokemon();
 
-  carregarPokemon(pokemonFiltrado);
-  
-});
+// function carregarPokemon(pokemons) {
+//   personagens = pokemons;
 
-document.getElementById("ordenarPokemons").addEventListener("change", function() {
-  const ordem = Array.from(document.querySelectorAll("#ordenarPokemons")).map(function (element) {
-    return element.value;
-  });
-  selectOrderPokemon(ordem);
-}
-);
+//   const showPokemons = document.getElementById("principal");
+//   let layout = "";
+//   showPokemons.innerHTML = "";
+//   for (pokemon of pokemons) {
+//     layout += `
+//           <div id="pokemon" class="poke-card">
+//             <div class="infos">
+//               <img src ="${pokemon.img}"/>
+//               <div id = "name">
+//                 ${pokemon.name}
+//               </div>
+//               <p class="subtitle">Tipo:</p>
+//               ${pokemon.type}
+//               <p class="subtitle">Fraquezas:</p>
+//               ${pokemon.weaknesses}
+//             </div>
+//           </div>`;
+//   };
+//   showPokemons.innerHTML = layout;
+// }
+// carregarPokemon(personagens);
 
-function selectOrderPokemon() {
-  let orderPokemon = document.getElementById("ordenarPokemons").value;
-  let orderList = window.ordenPokemons(orderPokemon, personagens);
-  carregarPokemon(orderList);
-}
+// document.getElementById("filter").addEventListener("click", function() {
+//   const types = Array.from(document.querySelectorAll(".type:checked")).map(function (element) {
+//     //foi criado um array, no qual estão os pokemon filtrados, a partir do array onde estão todos os pokemon
+//     //queryselectorall retornou uma lista com os elementos especificados(tipos checados pelo usuário)
+//     // map foi chamado para buscar o tipo dentro desse novo array
+//     return element.value;
+//   });
+//   filtrarPorTipo(POKEMON.pokemon, types);
+//   const pokemonFiltrado = app.filtrarPorTipo(POKEMON.pokemon, types);
 
-function showTypes() {
-  let qtTypes = app.pokeCalc(personagens);
-  for (tipo in qtTypes) {
-    document.getElementById("type"+tipo).innerHTML = qtTypes[tipo];
-  }
+//   carregarPokemon(pokemonFiltrado);
 
-}
+// });
 
-showTypes();
+// document.getElementById("ordenarPokemons").addEventListener("change", function() {
+//   const ordem = Array.from(document.querySelectorAll("#ordenarPokemons")).map(function (element) {
+//     return element.value;
+//   });
+//   selectOrderPokemon(ordem);
+// }
+// );
+
+// function selectOrderPokemon() {
+//   let orderPokemon = document.getElementById("ordenarPokemons").value;
+//   let orderList = window.ordenPokemons(orderPokemon, personagens);
+//   carregarPokemon(orderList);
+// }
+
+// function showTypes() {
+//   let qtTypes = app.pokeCalc(personagens);
+//   for (tipo in qtTypes) {
+//     document.getElementById("type"+tipo).innerHTML = qtTypes[tipo];
+//   }
+
+// }
+
+// showTypes();
 
